ca24fe233f65a3733de6e0f094b617136d43efc2
NoohAlavi <noohalavi@gmail.com>
2020-10-24 19:38:25 -0400

    Spawn citizen people

--- SpooktoberJam/World/World.gd
+++ SpooktoberJam/World/World.gd
@@ -3,5 +3,16 @@ extends Node2D
 export var citizens_array = [];
 export var citizen_scene: PackedScene;
 
+func _ready() -> void:
+	for i in range(10):
+		spawn_citizen();
+
 func spawn_citizen() -> void:
 	var new_citizen = citizen_scene.instance(); 
+	$Citizens.add_child(new_citizen);
+	citizens_array.append(new_citizen);
+
+
+func _on_CitizenSpawnTimer_timeout() -> void:
+	for i in range(3):
+		spawn_citizen();
