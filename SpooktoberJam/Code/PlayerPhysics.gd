e6520f6426dab33e2e35db09c119f674029f5a31
SkyanSam <samuelwiggins15@gmail.com>
2020-10-25 23:18:56 -0400

    Player Physics Update
    player cant go past bounds (min_x & max_x)

--- SpooktoberJam/Code/PlayerPhysics.gd
+++ SpooktoberJam/Code/PlayerPhysics.gd
@@ -4,6 +4,8 @@ export var move_speed = 300;
 export var vertical_speed = 300;
 export var overground_y = 0;
 export var underground_y = 500;
+export var max_x = 100;
+export var min_x = -100;
 var vertical_mode = 0;
 var velocity = Vector2()
 var touching_vent = false
@@ -23,6 +25,8 @@ func _physics_process(_delta):
 	match (vertical_mode):
 		0:
 			velocity.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * move_speed; #Moving
+			if ((velocity.x > 0 && position.x > max_x) || (velocity.x < 0 && position.x < min_x)):
+				velocity.x = 0; # Restrict Player By Bounds
 		-1:
 			velocity.x = 0;
 			if (position.y < overground_y): 
