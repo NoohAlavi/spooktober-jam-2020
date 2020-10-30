3e583f14bc86347745d543ae13807a263c68bec2
SkyanSam <samuelwiggins15@gmail.com>
2020-10-29 23:45:21 -0400

    Implemented Art for Buildings, Sidewalks, Newspaper Stands
    - fixed noohs bad citizen movement code (nooh needs to fix his code.. they shouldn't all be walking to police station)
    - implemented art for buildings
    - implemented art for sidewalks
    - implemented art for newspaper stands

--- SpooktoberJam/Citizen/Citizen.gd
+++ SpooktoberJam/Citizen/Citizen.gd
@@ -9,20 +9,22 @@ export var reached_destination: bool = true;
 var rand_nums = [2, 4, 6, 8]
 
 func _ready() -> void:
+	speed = (randf()*40) + 20
 	connect("body_entered", self, "_on_body_entered")
 	connect("body_entered", self, "_on_body_exited")
 
 func _process(_delta: float) -> void:
 	$"Exclamation Mark".visible = is_witness;
 	
+	if is_touching_player and Input.is_action_just_pressed("interact"):
+		if is_inside_tree():
+			queue_free()
+		else:
+			call_deferred("free")
+	
 func _physics_process(delta: float) -> void:
 	move(delta);
-	if is_touching_player and Input.is_action_pressed("interact"):
-		print("Interact")
-		free()
-		
-	if Input.is_action_pressed("interact"):
-		print("Interact Button")
+	
 
 func _on_body_entered(body: PhysicsBody2D) -> void:
 	if body.name == "Player":
@@ -34,7 +36,7 @@ func _on_body_exited(body: PhysicsBody2D) -> void:
 		is_touching_player = false
 		print("is_touching_player" + str(is_touching_player) + name);
 	
-func move(dt):
+func oldmove(dt):
 	velocity.x = 0;
 	if is_witness:
 		if position.x > -2000:
@@ -55,3 +57,23 @@ func move(dt):
 		#randomly move around
 		pass
 	position += (velocity * dt);
+
+func move(dt):
+	velocity.x = 0;
+	if reached_destination:
+		reached_destination = false;
+		var randX = rand_range(-1500, 1500) * pow(rand_nums[rand_range(0, 3)], speed);
+		if position.x < randX:
+			velocity.x += speed;
+		if position.x > randX:
+			velocity.x -= speed;
+		if position.x == randX:
+			reached_destination = true;
+	else:
+		if position.x > -2000:
+			velocity.x -= speed;
+		else:
+			velocity.x += speed;
+	
+	position += (velocity * dt);
+
--- SpooktoberJam/Code/NewspaperStand.gd
+++ SpooktoberJam/Code/NewspaperStand.gd
@@ -21,12 +21,16 @@ func empty():
 func fill():
 	newspaperCount = totalNewspaperCount;
 
+# Images
 func load_images():
 	for number in range(0,4):
 		images.append(load("res://Art/newspaper" + str(number) + ".png"));
 
 func update_images():
 	$Sprite.texture = images[newspaperCount];
+
+# Player
+var is_touching_player;
 
 # TIMER
 export var cooldownTime = 30
@@ -44,8 +48,12 @@ func _process(delta):
 	if (timer >= cooldownTime):
 		fill()
 		timer = 0
+		
+	if is_touching_player && Input.is_action_just_pressed("interact"):
+		print("Empty Newspaper")
+		empty();
 
-	timer += delta
+	timer += delta;
 
 func _on_Area2D_area_entered(area):
 	if (area.name.left(8) == "@Citizen" || area.name.left(7) == "Citizen"):
@@ -53,6 +61,18 @@ func _on_Area2D_area_entered(area):
 		if (newspaperCount > 0):
 			print("newspaperCount -= 1");
 			newspaperCount -= 1;
+	
+	
 
 func _on_Area2D_area_exited(area):
-	pass # Replace with function body.
+	pass
+
+
+func _on_NewspaperStand_body_entered(body):
+	if (body.name == "Player"):
+		is_touching_player = true;
+
+
+func _on_NewspaperStand_body_exited(body):
+	if (body.name == "Player"):
+		is_touching_player = false;
